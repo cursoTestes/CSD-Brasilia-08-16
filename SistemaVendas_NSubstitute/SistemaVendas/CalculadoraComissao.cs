cda691011d9c5696b396cb3975ab761a6efa3351
cursoTestes <cfc@ufrj.br>
2016-08-17 16:56:34 -0300

    CSD-Brasilia
    Import inicial.

---
+++ SistemaVendas_NSubstitute/SistemaVendas/CalculadoraComissao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaVendas
+{
+    public class CalculadoraComissao
+    {
+        public const decimal COMISSAO_FAIXA_2 = 6;
+
+        public decimal CalculaValorTotalComissao(decimal valor_venda)
+        {
+            if (valor_venda > 10000)
+            {
+                return (valor_venda / 100) * COMISSAO_FAIXA_2;
+            }
+            else {
+                return (valor_venda / 100) * 5;
+            }
+        }
+    }
+}
---
+++ SistemaVendas_NSubstitute/TesteVendas/TesteCalculoComissao.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaVendas;
+
+namespace TesteVendas
+{
+    [TestClass]
+    public class TesteCalculoComissao
+    {
+       [TestMethod]
+        public void Teste_entrando_com_100k_retorna_6k()
+        {
+           //variaveis Assert
+            decimal valor_venda = 100000;
+            decimal valor_esperado = 6000;
+
+           //chama o metodo Act
+            CalculadoraComissao calculadoraComissao = new CalculadoraComissao();
+            decimal retorno = calculadoraComissao.CalculaValorTotalComissao(valor_venda);
+
+            Assert.AreEqual(valor_esperado, retorno);
+        }
+
+       [TestMethod]
+       public void Teste_entrando_com_9999_retorna_499Reaise95cents()
+       {
+           //variaveis Assert
+           decimal valor_venda = 9999;
+           decimal valor_esperado = 499.95M;
+
+           //chama o metodo Act
+           CalculadoraComissao calculadoraComissao = new CalculadoraComissao();
+           decimal retorno = calculadoraComissao.CalculaValorTotalComissao(valor_venda);
+
+           Assert.AreEqual(valor_esperado, retorno);
+       }
+
+       [TestMethod]
+       public void Teste_entrando_com_10k_retorna_500()
+       {
+           //variaveis Assert
+           decimal valor_venda = 10000;
+           decimal valor_esperado = 500;
+
+           //chama o metodo Act
+           CalculadoraComissao calculadoraComissao = new CalculadoraComissao();
+           decimal retorno = calculadoraComissao.CalculaValorTotalComissao(valor_venda);
+
+           Assert.AreEqual(valor_esperado, retorno);
+      }
+
+    }
+}
